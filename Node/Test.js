185d3bcc6b565c1ad1040d16a5f861923e28bc0f
isaacpeters <dabomb11111@gmail.com>
2018-01-30 20:05:17 -0800

    Updated with example 5

--- Node/Test.js
+++ Node/Test.js
@@ -1,35 +1,18 @@
-function deepEqual(inputA, inputB) {
-    if ((typeof inputA == "object" && inputA != null) && (typeof inputB == "object" && inputB != null)) {
-        for (var propA in inputA) {
-            var bothHave = false;
-            for (var propB in inputB) {
-                if ((typeof inputA[propA] == "object" && inputA[propA] != null) && (typeof inputB[propB] == "object" && inputB[propB] != null)) {
-                    if (deepEqual(inputA[propA], inputB[propB])) {
-                        bothHave = true;
-                        break;
-                    }
-                } else if (inputA[propA] === inputB[propB]) {
-                    bothHave = true;
-                    break;
-                }
-            }
-            if (!bothHave) {
-                return false;
-            }
-        }
-    } else {
-        if (inputA !== inputB) {
-            return false;
-        }
-    }
-
-    return true;
+function buildList(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+//         var item = function(x) { return 'item' + list[x]}(i);
+        result.push( function(x) { return function() {console.log('item' + list[x] +' ' + list[x])} }(i) );
+    }
+    return result;
+}
+ 
+function testList() {
+    var fnlist = buildList([2,6,6]);
+    // using j only to help prevent confusion - could use i
+    for (var j = 0; j < fnlist.length; j++) {
+        fnlist[j]();
+    }
 }
 
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
+testList();
