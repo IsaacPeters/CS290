6be98d66fae458f6d470cf609ce33e714fad8332
isaacpeters <dabomb11111@gmail.com>
2018-02-23 22:59:24 -0800

    change port

--- GET and POST checker/index.js
+++ GET and POST checker/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 6875);
+app.set('port', 1845);
 
 app.get('/',function(req,res){
     var parameters = [];
