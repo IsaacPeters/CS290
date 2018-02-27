025bf243283fbfb44c4723a5b0320bf734174672
crazypiemansam <dabomb11111@gmail.com>
2018-02-27 11:09:47 -0800

    Need to restart

---
+++ Project/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 1845);
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
